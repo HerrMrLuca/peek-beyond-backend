61ef841d2f6d5cf0779b3185b6e0d624ef2ac184
ElisabethSchuhmann <s2110238065@fhooe.at>
2024-01-06 21:40:52 +0100

    Update project.ts

--- strapi/src/api/project/controllers/project.ts
+++ strapi/src/api/project/controllers/project.ts
@@ -4,148 +4,181 @@
 
 import {factories} from '@strapi/strapi'
 
+function getLocale(ctx) {
+  let locale = ctx.query.locale;
+
+  if (locale != "de" && locale != "en") {
+    locale = "de"
+  }
+  return locale;
+}
+
 export default factories.createCoreController('api::project.project', ({strapi}) => ({
 
-    async find(ctx) {
-        const projects = await strapi.entityService
-            .findMany('api::project.project', {
-                    populate: [
-                        'thumbnail'
-                    ],
-                }
-            );
+  async find(ctx) {
+    let locale = getLocale(ctx);
 
-        ctx.body = {
-            data: projects
-        };
-    },
-    async findLatest(ctx) {
-        const projects = await strapi.entityService
-            .findMany('api::project.project', {
-                    sort: {
-                        createdAt: 'desc'
-                    },
-                    populate: [
-                        'thumbnail'
-                    ],
-                    limit: 30
-                }
-            );
+    const projects = await strapi.entityService
+      .findMany('api::project.project', {
+          populate: [
+            'thumbnail'
+          ],
+          locale: locale,
+        }
+      );
 
-        ctx.body = {
-            data: projects
-        };
-    },
+    ctx.body = {
+      data: projects
+    };
+  },
 
-    async findOne(ctx) {
-        console.log(ctx.params.id)
-        const project = await strapi.entityService
-            .findOne('api::project.project', ctx.params.id, {
-                    populate: '*'
-                }
-            );
+  async findLatest(ctx) {
+    let locale = getLocale(ctx);
 
-        ctx.body = {
-            data: project
-        };
-    },
+    const projects = await strapi.entityService
+      .findMany('api::project.project', {
+          populate: [
+            'thumbnail'
+          ],
+          locale: locale,
+          orderBy: {
+            createdAt: 'desc',
+          },
+          limit: 30,
+        },
+      );
 
-    async projectsByCategory(ctx) {
-        const projects = await strapi.entityService
-            .findMany('api::project.project', {
-                    populate: {
-                        supervisor: true,
-                        degree_and_semester: true,
-                        main_category: true,
-                        thumbnail: true,
-                        members: true,
-                        gallery: true,
-                        sub_categories: true/* {
+    ctx.body = {
+      data: projects
+    };
+  },
+
+  async findOne(ctx) {
+    const locale = ctx.query.locale;
+
+    let project = await strapi.entityService
+      .findOne('api::project.project', ctx.params.id, {
+          populate: '*'
+        },
+      );
+
+    if (locale == "en") {
+      let id = project.localizations[0].id;
+
+      project = await strapi.entityService
+        .findOne('api::project.project', id, {
+            populate: '*'
+          },
+        );
+    }
+
+    ctx.body = {
+      data: project
+    };
+  },
+
+  async projectsByCategory(ctx) {
+    let locale = getLocale(ctx);
+
+    const projects = await strapi.entityService
+      .findMany('api::project.project', {
+          populate: {
+            supervisor: true,
+            degree_and_semester: true,
+            main_category: true,
+            thumbnail: true,
+            members: true,
+            gallery: true,
+            sub_categories: true/* {
               filters: {
                 id: {
                   $eq: ctx.params.id,
                 },
               },
             }*/,
-                    },
-                    filters: {
-                        $or: [
-                            {
-                                main_category: {
-                                    id: {
-                                        $eq: ctx.params.id
-                                    }
-                                }
-                            },
-                            {
-                                sub_categories: {
-                                    id: {
-                                        $eq: ctx.params.id
-                                    }
-                                }
-                            }
-                        ]
-                    },
+          },
+          locale: locale,
+          filters: {
+            $or: [
+              {
+                main_category: {
+                  id: {
+                    $eq: ctx.params.id
+                  }
                 }
-            );
-
-        ctx.body = {
-            data: projects
-        };
-    },
-
-    async projectsByMainCategory(ctx) {
-        console.log(ctx.params.id)
-
-        const projects = await strapi.entityService
-            .findMany('api::project.project', {
-                    populate: '*',
-                    filters: {
-                        main_category: {
-                            id: {
-                                $eq: ctx.params.id
-                            }
-                        }
-                    },
+              },
+              {
+                sub_categories: {
+                  id: {
+                    $eq: ctx.params.id
+                  }
                 }
-            );
-        ctx.body = {
-            data: projects
-        };
-    },
-
-    async projectsByMainCategories(ctx) {
-        let result;
-
-        const categories = await strapi.entityService
-            .findMany('api::category.category', {
-                sort: [{
-                    id: 'desc'
-                }]
-            });
-
-        result = categories;
-
-        for (const categoriesKey in categories) {
-            const projects = await strapi.entityService
-                .findMany('api::project.project',
-                    {
-                        filters: {
-                            main_category: {
-                                id: categoriesKey
-                            }
-                        },
-                        populate: '*',
-                        limit: 20
-                    }
-                );
-
-            //NOTE .projects löschen dann erhält man keine Category Infos mehr
-            result[categoriesKey]/*.projects*/ = projects;
+              }
+            ]
+          },
         }
+      );
 
-        ctx.body = {
-            data: result
-        };
-    },
+    ctx.body = {
+      data: projects
+    };
+  },
+
+  async projectsByMainCategory(ctx) {
+    let locale = getLocale(ctx);
+
+    const projects = await strapi.entityService
+      .findMany('api::project.project', {
+          populate: '*',
+          locale: locale,
+          filters: {
+            main_category: {
+              id: {
+                $eq: ctx.params.id
+              }
+            }
+          },
+        }
+      );
+    ctx.body = {
+      data: projects
+    };
+  },
+
+  async projectsByMainCategories(ctx) {
+    let result;
+    let locale = getLocale(ctx);
+
+    const categories = await strapi.entityService
+      .findMany('api::category.category', {
+        sort: [{
+          id: 'desc'
+        }]
+      });
+
+    result = categories;
+
+    for (const categoriesKey in categories) {
+      const projects = await strapi.entityService
+        .findMany('api::project.project',
+          {
+            filters: {
+              main_category: {
+                id: categoriesKey
+              }
+            },
+            populate: '*',
+            locale: locale,
+            limit: 20
+          }
+        );
+
+      //NOTE .projects löschen dann erhält man keine Category Infos mehr
+      result[categoriesKey]/*.projects*/ = projects;
+    }
+
+    ctx.body = {
+      data: result
+    };
+  },
 }));
